79db105f6cc7bb6fbca4cef07c8da2f36c74b2b8
ShinDongHwi <ehdgnl8940@naver.com>
2022-04-02 15:26:40 +0900

    feat: randomColor Funtion 추가

--- app/src/main/java/com/example/jetpackcompose_modifier_randomcolor/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_modifier_randomcolor/MainActivity.kt
@@ -7,18 +7,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose_modifier_randomcolor.ui.theme.JetpackCompose_Modifier_RandomColorTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackCompose_Modifier_RandomColorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                }
+
             }
         }
     }
+}
+
+// 랜덤 칼라 가져오기
+fun Color.Companion.random(): Color {
+    val red = Random.nextInt(256)
+    val green = Random.nextInt(256)
+    val blue = Random.nextInt(256)
+    return Color(red, green, blue)
 }
